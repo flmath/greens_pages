5033756112f874f30e1d7a54c5cfbd6654dd6da3
flmath <flmathematic@gmail.com>
2019-03-11 10:56:47 +0100

    Test update

--- src/index.test.js
+++ src/index.test.js
@@ -4,6 +4,6 @@ import MainFrame from './MainFrame';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Game />, div);
+  ReactDOM.render(<MainFrame />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
