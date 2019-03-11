e5c37d882705bdba41b1a252cb1b360e37d7f2c6
flmath <flmathematic@gmail.com>
2019-03-11 05:09:38 +0100

    wsp

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Game from './Game';
-
+ 
 ReactDOM.render(
   <Game />,
   document.getElementById('root')
