a6f71423a3f112d18952349c32160725ac5cc921
Mathias Green <flmathematic@gmail.com>
2019-03-11 04:43:28 +0100

    Remove button

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Game from './Game';
-import { Button } from 'reactstrap';
+
 ReactDOM.render(
   <Game />,
   document.getElementById('root')
