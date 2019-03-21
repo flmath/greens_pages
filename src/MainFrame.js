b11ae96b1baa3fd7ce8f21956aa3a38f44379aab
flmath <flmathematic@gmail.com>
2019-03-21 08:53:24 +0100

    add the cv butterfly

--- src/MainFrame.js
+++ src/MainFrame.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import {Navbar} from 'reactstrap';
+import {Navbar, NavbarBrand} from 'reactstrap';
 import CvMain from './cv/CvMain';
-
+import brandCv from './pics/butterflycv.png';
 
 function MainFrame(props) {
       return (
@@ -9,7 +9,9 @@ function MainFrame(props) {
         <div id="topdiv">
 
         <Navbar id="navbar" >
-
+          <NavbarBrand href="/" className="ml-auto">
+            <img id="cv_image" src={brandCv} alt="CV"/>
+            </NavbarBrand>
         </Navbar>
         <div id="the_body">
           <CvMain></CvMain>
