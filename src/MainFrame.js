b1c4f7ddd9eb603480ee979ad311705b2a1dd1d0
flmath <flmathematic@gmail.com>
2019-05-15 20:11:53 +0200

    removed custom history

--- src/MainFrame.js
+++ src/MainFrame.js
@@ -5,93 +5,32 @@ import "./css/leaf.css";
 
 
 import PayloadBody from './PayloadBody'
-import { createBrowserHistory } from 'history';
 const MainNavbar = React.lazy(() => import('./MainNavbar'));
-
-
 
 class MainFrame extends React.Component {
   constructor(props){
     super(props)
-    const initState = {bodyPayloadType: 2, //1 == CV; 2 == List; 3 == subpage; 4 == readme
+    this.state = {bodyPayloadType: 2, //1 == CV; 2 == List; 3 == subpage; 4 == readme
                       filterText: "",
                       href:  ""}
-    let theHistory = createBrowserHistory()
-    theHistory.listen(this.popHistory.bind(this));
-    theHistory.push({state: initState})
-    this.state = {...initState,
-                  history: theHistory
-                }
   }
-
-popHistory(location, action){
-     // console.log(location.state)
-     // console.log(location.pathname!=="/")
-     // console.log(
-     //             `The current URL is ${location.pathname}${location.search}${location.hash}`
-     //           );
-  if(location.state!==undefined & action==='POP')
-  {
-  this.setState(location.state)
-}
-else if(location.state===undefined & location.pathname!=="/" & action==='POP')
-{
-// browser moves automatically when url changes
-}
-}
-
-  setStateWithHistoryPush(stateUpdate){
-    this.setState((prevState, props) => {
-    const stateToStore = getCurrentHistoryState(prevState)
-    prevState.history.push({state:
-      {...stateToStore,
-       ...stateUpdate}});
-    return  stateUpdate
-  })}
-
-  setStateWithHistoryReplace(stateUpdate){
-    this.setState((prevState, props) => {
-    const stateToStore = getCurrentHistoryState(prevState)
-    prevState.history.replace({state:
-      {...stateToStore,
-       ...stateUpdate}});
-      return  stateUpdate
-  })}
-
-  setStateWithHistoryRemove(stateUpdate){
-
-    this.setState((prevState, props) => {
-    const stateToStore = getCurrentHistoryState(
-                                {...prevState,
-                                 ...stateUpdate})
-    prevState.history.push({state: stateToStore});
-    return stateUpdate
-  })}
 
   handleInput(event) {
-    if (event.target.value==="" & this.state.filterText!=="") {
-      this.setStateWithHistoryRemove({filterText: ""})
-    } else
-    if (this.state.filterText==="") {
-      // console.log(event.target.value==="")
-      this.setStateWithHistoryPush({filterText: event.target.value})
+      this.setState({filterText: event.target.value})
     }
-    else{
-    this.setStateWithHistoryReplace({filterText: event.target.value})
-}
-  }
 
   routeToPage(new_href){
-    this.setStateWithHistoryPush({bodyPayloadType: 3,
+    this.setState({bodyPayloadType: 3,
                    href:  new_href})}
 
   switchState() {
         const theNextType = nextPayloadType(this.state.bodyPayloadType)
-        this.setStateWithHistoryPush({bodyPayloadType: theNextType})
+        this.setState({bodyPayloadType: theNextType})
             }
-  componentWillMount() { window.scrollTo(0, 0) }
-  render() {
 
+  componentWillMount() { window.scrollTo(0, 0) }
+
+  render() {
       const theNextType = nextPayloadType(this.state.bodyPayloadType)
       return (
       <div id="topdiv">
@@ -112,13 +51,6 @@ else if(location.state===undefined & location.pathname!=="/" & action==='POP')
       );
   }}
 
-  function getCurrentHistoryState(thisState){
-                return {
-                bodyPayloadType: thisState.bodyPayloadType,
-                filterText: thisState.filterText,
-                href:  thisState.href
-                }
-  }
 
 function nextPayloadType(currentType)
 {
