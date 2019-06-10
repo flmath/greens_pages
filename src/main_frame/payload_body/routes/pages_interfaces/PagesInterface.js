4d9ebbc88da56df5c8d39f767edc6076fb64cc8a
flmath <flmathematic@gmail.com>
2019-06-10 09:27:42 +0200

    PublicIntrerface refactoring

--- src/main_frame/payload_body/routes/pages_interfaces/PagesInterface.js
+++ src/main_frame/payload_body/routes/pages_interfaces/PagesInterface.js
@@ -1,13 +1,30 @@
 import React  from 'react';
 import { withRouter} from "react-router-dom"
 import PublicPagesInterface from "./PublicPagesInterface"
+import json_data from '../../../../postlist.json';
+import NoMatch from '../NoMatch';
+
 
 function PagesInterface(props) {
 
+  const checkPost = createCheckPost(props.match.params.href)
+  const thePost = json_data.the_list.find(checkPost)
 
+   if(thePost){
             return(
-            <PublicPagesInterface></PublicPagesInterface>)
+            <PublicPagesInterface postHref={thePost.href.substring(1)}></PublicPagesInterface>)
 
+}          else{
+            return(
+            <NoMatch></NoMatch>)
+              }
 }
+
+function createCheckPost(href){
+
+return function checkPost(aPost){
+  return aPost.href.substring(1) === href
+}}
+
 
 export default withRouter(PagesInterface);
