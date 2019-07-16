658946075f4168ee49a7dd056c07131284d9cdfb
flmath <flmathematic@gmail.com>
2019-07-16 09:05:14 +0200

    Fixed tags matching fot PostList

--- src/main_frame/payload_body/routes/SupportFunctions.js
+++ src/main_frame/payload_body/routes/SupportFunctions.js
@@ -1,8 +1,7 @@
 function filterPostFun(filterText){
   return function(aPost){
     let is_in_title = aPost.title.toLowerCase().indexOf(filterText.toLowerCase())!==-1;
-    let is_in_tags  = aPost.tags.map(toLowerCaseFun).indexOf(filterText.toLowerCase())!==-1;
-
+    let is_in_tags  = aPost.tags.map(toLowerCaseFun).some((post) => post.indexOf(filterText.toLowerCase())!==-1);
     return (is_in_title || is_in_tags)}
 
 }
@@ -11,4 +10,4 @@ function toLowerCaseFun(str){
   return str.toLowerCase()
 }
 
-export {filterPostFun} 
+export {filterPostFun}
