64f9c8b7a861e526d6e56340029747593ddc8e1c
flmath <flmathematic@gmail.com>
2019-08-18 23:30:07 +0200

    changed link to cream cheese

--- src/private_pages/Recipes.js
+++ src/private_pages/Recipes.js
@@ -23,16 +23,16 @@ function Recipes(props) {
             <RecipeRow header="Zucchini Crumpet"
               ingredients = {["A small onion", "A clove of garlic", "Olive oil",
                  "A zuccini", "A teaspoon of bouillon powder", "50g of cream cheese"]}
-              instructions = {["Put the onion, garlic into a thermomix and blend them (15s/speed 5).",
-                  "Add oil. Run the thermomix with on Varoma speed 1 for 2 minutes.",
-                "Clean the zucchini and cut zucchini (do not remove the skin). "+
-                 "Add to Thermomix: zuccini and water in 1:1 proportions (weightwise) and bouillon. "+
+              instructions = {["Put the onion, garlic into a Thermomix and blend them (15s/speed 5).",
+                  "Add oil. Run the thermomix with Varoma, speed 1 for 2 minutes.",
+                "Clean and cut the zucchini (do not remove the skin). "+
+                 "Add to thermomix: the zuccini and water in 1:1 proportions (weightwise) and bouillon. "+
                  "Run the thermomix with speed 4, temperature 100&deg;C for 15 minutes.",
                  "Run the thermomix increamentally speed 4, 5, 6, 7, 8 for 30 seconds each.",
                  "Add the cream cheese to Thermomix with speed 4, for 10 seconds."
                 ]}
             note = {"NOTE: Prefered cream cheese of my wife: "+
-                    "<a href='http://piatnica.com.pl/produkty/twoj-smak-naturalny-135-g'>"+
+                    "<a href='http://piatnica.com.pl/en/products/cream-cheeses'>"+
                     "Piatnica cream cheese.</a>"}
                  >
             </RecipeRow>
