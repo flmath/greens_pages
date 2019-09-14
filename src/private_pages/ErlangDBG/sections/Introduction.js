c3c07b87daa23fd6ee4271461e982af78eacbefc
flmath <flmathematic@gmail.com>
2019-09-15 01:00:10 +0200

    changed intro to dbg erlang

--- src/private_pages/ErlangDBG/sections/Introduction.js
+++ src/private_pages/ErlangDBG/sections/Introduction.js
@@ -4,14 +4,11 @@ export default function Introduction(props) {
   return (
     <div className="my_md_container">
         <p>
-When you ask an Erlang acolyte why this language is so great you will hear about functional programming advantages
-or the beauty of the actor model. But what makes Erlang worth spending time is its maturity.
-      </p>
-      <p>An important aspect of this maturity is Virtual Machine you can rely on and the OTP platform.
-Those two features provide us with dbg library that is part of OTP,
-which means normally it is shipped even on production servers.
-      Unfortunately, not everyone finds the tool user-friendly and when used wrong way,
-       it can do a lot of harm to your the system (see <a href="#CustomHandler">Custom Handler: Introduction</a>).
+      An important aspect of the Erlang/OTP platforms maturity is the presence of native debug library.
+      Unfortunately, the dbg module is not only steep to learn but also can be very harmful if misused (see <a href="#CustomHandler">Custom Handler: Introduction</a>).
+      In this post, I show how to use the tool, avoid pitfalls and extract the needed information.
+      The article is composed of scenarios starting with the most basic and then expanding it with the usage of the module different caveats.
+      Each scenario uses very simple gen_statem as target practice, the setup of it is in the following chapter.
         </p>
     </div>
   );
